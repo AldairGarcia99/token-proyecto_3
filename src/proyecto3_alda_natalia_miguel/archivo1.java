63b81e22b7650357ec66d07a6ea7ec994c0d5eb7
aldair <aldairgarciap15@gmail.com>
2024-04-05 13:02:42 +0200

    commit 2 de archivo 1 y 2 commit 1 de archivo 3

--- src/proyecto3_alda_natalia_miguel/archivo1.java
+++ src/proyecto3_alda_natalia_miguel/archivo1.java
@@ -4,6 +4,7 @@ public class archivo1 {
 
 	public static void main(String[] args) {
 		System.out.println("LOLAZO1");
+		System.out.println("LOLAZO2");
 	}
 
 }
--- src/proyecto3_alda_natalia_miguel/archivo3.java
+++ src/proyecto3_alda_natalia_miguel/archivo3.java
@@ -1,10 +1,9 @@
 package proyecto3_alda_natalia_miguel;
 
-public class archivo2 {
+public class archivo3 {
 
 	public static void main(String[] args) {
 		System.out.println("LOLAZO1");
-		System.out.println("LOLAZO2");
 	}
 
 }
